7a44ef8b398f19580b5406165667a31208e04be9
Kola Grey <kolagrey@gmail.com>
2019-09-10 20:57:11 +0100

    Minor Update

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require('./src/app');
 const PORT = 3000;
 
 server.listen(PORT, () => {
-    console.log(`Server runnin on PORT ${PORT}`);
+    console.log(`Test Service running on PORT ${PORT}`);
 });
